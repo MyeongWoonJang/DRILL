2a52ec76275344e926060994e96ec8a503529dea
MyeongWoonJang <73771162+MyeongWoonJang@users.noreply.github.com>
2021-09-14 19:13:22 +0900

    DRILL 05 완성
    2020180033 장명운
    https://www.youtube.com/watch?v=TkvQ_tNxbAM

---
+++ 05/t.py
@@ -0,0 +1,30 @@
+import random
+import turtle
+
+move_px = 50
+
+def restart():
+    turtle.reset()
+
+def upmove():
+    turtle.setheading(90)
+    turtle.forward(move_px)
+
+def leftmove():
+    turtle.setheading(180)
+    turtle.forward(move_px)
+
+def rightmove():
+    turtle.setheading(0)
+    turtle.forward(move_px)
+
+def downmove():
+    turtle.setheading(270)
+    turtle.forward(move_px)
+
+turtle.onkey(upmove, 'w')
+turtle.onkey(leftmove, 'a')
+turtle.onkey(downmove, 's')
+turtle.onkey(rightmove, 'd')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
