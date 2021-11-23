8adb6f3fa6729164a7d650d69c48028a3379d7e5
MyeongWoonJang <73771162+MyeongWoonJang@users.noreply.github.com>
2021-11-23 17:20:01 +0900

    업데이트

--- 14/Lecture14_Collision/main_state.py
+++ 14/Lecture14_Collision/main_state.py
@@ -9,7 +9,7 @@ import game_world
 
 from boy import Boy
 from grass import Grass
-from ball import Ball
+from ball import Ball, BigBall
 from bird import Bird
 
 name = "MainState"
@@ -21,7 +21,13 @@ big_balls = []
 
 
 def collide(a, b):
-    # fill here
+    a_xmin, a_ymin, a_xmax, a_ymax = a.get_bb()
+    b_xmin, b_ymin, b_xmax, b_ymax = b.get_bb()
+
+    if a_xmin > b_xmax: return False
+    if b_xmin > a_xmax: return False
+    if a_ymin > b_ymax: return False
+    if b_ymin > a_ymax: return False
     return True
 
 
@@ -38,6 +44,10 @@ def enter():
 
     for _ in range(5):
         game_world.add_object(Bird(), 1)
+
+    global balls
+    balls = [Ball() for _ in range(10)] + [BigBall() for _ in range(10)]
+    game_world.add_objects(balls, 1)
 
     # fill here for balls
 
@@ -62,7 +72,7 @@ def handle_events():
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-                game_framework.quit()
+            game_framework.quit()
         else:
             boy.handle_event(event)
 
@@ -70,6 +80,16 @@ def handle_events():
 def update():
     for game_object in game_world.all_objects():
         game_object.update()
+    for ball in balls:
+        if collide(boy, ball):
+            balls.remove(ball)
+            game_world.remove_object(ball)
+
+    for ball in balls:
+        if collide(grass, ball):
+            ball.stop()
+
+    delay(1.5)
 
     # fill here for collision check
 
